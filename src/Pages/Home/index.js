d3a9f153011444db97b0fe75db705b82ea6638fd
João Guedes <junior.guedes@academico.ifrn.edu.br>
2021-09-28 04:00:36 -0300

    Base do projeto

---
+++ src/Pages/Home/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default function Home() {
+    return (
+        <View>
+            <Text style={{ marginTop: 60, fontSize: 25, fontFamily: 'Anton_400Regular' }}>Página Home</Text>
+        </View>
+    );
+}
---
+++ src/router.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import Home from './Pages/Home';
+import Detail from './Pages/Detail';
+
+const Stack = createNativeStackNavigator();
+
+function Routes() {
+    return (
+        <NavigationContainer>
+            <Stack.Navigator>
+                <Stack.Screen
+                    name="Home"
+                    component={Home}
+                    options={{
+                        headerShown: false
+                    }}
+                />
+
+                <Stack.Screen
+                    name="Detail"
+                    component={Detail}
+                />
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
+};
+
+export default Routes;
